969907a7c109214cbcacc6d4812d0dc2715fb88c
Kristian Kantola <k9958@student.jamk.fi>
2023-12-15 17:34:57 +0200

    fix navlinks, had nested <a> from both bootstrap and react-router

--- FinalAssignment/shop_app/src/Navigation.jsx
+++ FinalAssignment/shop_app/src/Navigation.jsx
@@ -3,7 +3,7 @@ import { Container, Navbar, NavDropdown, Offcanvas, Nav } from 'react-bootstrap'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
-const imageURL ="/komponentti/"
+const imageURL = "/komponentti/"
 
 const Navigation = () => {
   // eslint-disable-next-line no-unused-vars
@@ -12,9 +12,9 @@ const Navigation = () => {
   const expand = "lg"
   return (
     <>
-      <Navbar expand={expand} sticky="top"  className="bg-body-tertiary navbar">
+      <Navbar expand={expand} sticky="top" className="bg-body-tertiary navbar">
         <Container fluid
-        className='container'
+          className='container'
           style={{ justifyContent: "start" }}
         >
           <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`}
@@ -22,19 +22,17 @@ const Navigation = () => {
             className='custom-toggler'
             style={{ border: "none" }}
           />
-          <Link to={"/"}>
-            <Navbar.Brand href="/"
-            >
-              <img
-                alt=""
-                src={imageURL + "logo.svg"}
-                width="30"
-                height="30"
-                className="d-inline-block align-top"
-              />{' '}
-              Komponentti
-            </Navbar.Brand>
-          </Link>
+          <Navbar.Brand as={Link} to={"/"}
+          >
+            <img
+              alt=""
+              src={imageURL + "logo.svg"}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            Komponentti
+          </Navbar.Brand>
           <Navbar.Offcanvas
             id={`offcanvasNavbar-expand-${expand}`}
             aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
@@ -54,51 +52,50 @@ const Navigation = () => {
             </Offcanvas.Header>
             <Offcanvas.Body>
               <Nav className="justify-content-start flex-grow-1 pe-3">
-                <Link to={"/"}><Nav.Link href="/">Etusivu</Nav.Link></Link>
+                <Nav.Link as={Link} to={"/"}>Etusivu</Nav.Link>
                 <NavDropdown
                   title="Tuotteet"
                   id={`offcanvasNavbarDropdown-expand-${expand}`}
                 >
-                  <NavDropdown.Item>
-                    <Link to={"/categories/gpu"}>Näytönohjaimet</Link>
+                  <NavDropdown.Item as={Link} to={"/categories/gpu"}>
+                    Näytönohjaimet
                   </NavDropdown.Item>
-                  <NavDropdown.Item>
-                    <Link to={"/categories/cpu"}>Prosessorit</Link>
+                  <NavDropdown.Item as={Link} to={"/categories/cpu"}>
+                    Prosessorit
                   </NavDropdown.Item>
-                  <NavDropdown.Item>
-                    <Link to={"/categories/motherboard"}>Emolevyt</Link>
+                  <NavDropdown.Item as={Link} to={"/categories/motherboard"}>
+                    Emolevyt
                   </NavDropdown.Item>
-                  <NavDropdown.Item>
-                    <Link to={"/categories/case"}>Kotelot</Link>
+                  <NavDropdown.Item as={Link} to={"/categories/case"}>
+                    Kotelot
                   </NavDropdown.Item>
-                  <NavDropdown.Item>
-                    <Link to={"/categories/psu"}>Virtalähteet</Link>
+                  <NavDropdown.Item as={Link} to={"/categories/psu"}>
+                    Virtalähteet
                   </NavDropdown.Item>
-                  <NavDropdown.Item>
-                    <Link to={"/categories/ram"}>Muistit</Link>
+                  <NavDropdown.Item as={Link} to={"/categories/ram"}>
+                    Muistit
                   </NavDropdown.Item>
-                  <NavDropdown.Item>
-                    <Link to={"/categories/hdd-ssd"}>Kiintolevyt ja SSD:t</Link>
+                  <NavDropdown.Item as={Link} to={"/categories/hdd-ssd"}>
+                    Kiintolevyt ja SSD:t
                   </NavDropdown.Item>
                   {/* <NavDropdown.Divider /> */}
                 </NavDropdown>
               </Nav>
             </Offcanvas.Body>
           </Navbar.Offcanvas>
-          <Link to={"/cart"} style={{marginLeft:"auto"}}>
-            <Navbar.Brand href="/cart"
-              className='shopping-cart'
-            >
-              <img
-                alt=""
-                src={imageURL + "shoppingcart.svg"}
-                width="23"
-                height="30"
-                className="d-inline-block align-top"
-              />{' '}
+          <Navbar.Brand href="/cart"
+            as={Link} to={"/cart"} style={{ marginLeft: "auto" }}
+            className='shopping-cart'
+          >
+            <img
+              alt=""
+              src={imageURL + "shoppingcart.svg"}
+              width="23"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
 
-            </Navbar.Brand>
-          </Link>
+          </Navbar.Brand>
         </Container>
       </Navbar>
     </>
